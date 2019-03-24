73c7cb6c0f70dcb8a2360a90c7f6125516e1657b
Anikesh Patel <anikeshpatel4@gmail.com>
2019-03-24 09:20:14 +0530

    wire-up project

---
+++ src/ui/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default class Header extends React.Component {
+    render() {
+        return (
+            <header>
+                <nav>
+                    <div>
+                        <h1>Loverz</h1>
+                    </div>
+                </nav>
+            </header>
+        )
+    }
+}
+
